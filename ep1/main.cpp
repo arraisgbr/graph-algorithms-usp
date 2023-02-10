c06b1b8ff471b9b7998cde300033826d312ba9f1
Gabriel Arrais <arrais.gbr@usp.br>
2023-02-10 02:13:14 -0300

    tarjan with bugs

--- ep1/main.cpp
+++ ep1/main.cpp
@@ -2,6 +2,7 @@
 #pragma region
 #include <iostream>
 #include <cmath>
+#include <stack>
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #pragma endregion
@@ -27,6 +28,7 @@ using boost::make_iterator_range;
 using boost::vertices;
 using boost::out_edges;
 using boost::target;
+using boost::num_vertices;
 #pragma endregion
 
 // toValidOrFalse and toTrue functions
@@ -34,14 +36,85 @@ using boost::target;
 int toValidOrFalse(int var, int n){
     return abs(var) + n;
 }
+
 int toTrue(int var, int n){
     return var - n;
 }
 #pragma endregion
 
-void tarjan(Digraph &digraph, int *scc){
-    
+// tarjan
+#pragma region
+std::pair<bool, int> verifySat(int *scc, int n){
+    bool sat = true;
+    bool varUnsat;
+    for(int i = 0 ; i < n ; i++){
+        int iNeg = toValidOrFalse(i, n);
+        if(scc[i] == scc[iNeg]){
+            varUnsat = i;
+            sat = false;
+            break;
+        }
+    }
+    return std::make_pair(sat, varUnsat);
 }
+
+void tarjanR(Digraph &digraph, int vertex, int *scc, int *disc, int *low, std::stack<int> &stack, bool *inStack, int sccNum){
+    static int time = 0;
+    disc[vertex] = low[vertex] = ++time;
+    stack.push(vertex);
+    inStack[vertex] = true;
+
+    for(Arc arc : make_iterator_range(out_edges(vertex, digraph))){
+        Vertex to = target(arc, digraph);
+        if(disc[to] == -1){
+            tarjanR(digraph, to, scc, disc, low, stack, inStack, sccNum);
+            low[vertex] = std::min(low[vertex], low[to]);
+        }
+        else if(inStack[to])
+            low[vertex] = std::min(low[vertex], disc[to]);
+    }
+
+    //debug
+    // std::cout << "Chegou no stack" << std::endl;
+
+    if(disc[vertex] == low[vertex]){
+        while(stack.top() != vertex){
+            Vertex curr = stack.top(); stack.pop();
+            inStack[curr] = false;
+            scc[curr] = sccNum;
+        }
+        stack.pop();
+        scc[vertex] = sccNum;
+        inStack[vertex] = false;
+    }
+}
+
+std::pair<bool, int> tarjan(Digraph &digraph){
+    int n = num_vertices(digraph);
+    std::stack<int> stack;
+    
+    int sccNum = 0;
+    int *disc = new int[2*n]; 
+    int *low = new int[2*n]; 
+    bool *inStack = new bool[2*n]; 
+    int *scc = new int[2*n];
+    for(int i = 0 ; i < 2*n ; i++){
+        scc[i] = disc[i] = low[i] = -1;
+        inStack[i] = false;
+    }
+
+    for(int i = 0 ; i < 2*n ; i++){
+        if(disc[i] == -1){
+            sccNum++;
+            tarjanR(digraph, i, scc, disc, low, stack, inStack, sccNum);
+        }
+    }
+
+    std::pair<bool, int> ans = verifySat(scc, n);    
+
+    return ans;
+}
+#pragma endregion
 
 int main(){
 
@@ -54,43 +127,40 @@ int main(){
         int a, b; std::cin >> a >> b;
         // getting the positive value for the vertex
         if(a < 0) a = toValidOrFalse(a, n);
-        if(b < 0) b = toValidOrFalse(a, n);
+        else a -= 1;
+        if(b < 0) b = toValidOrFalse(b, n);
+        else b -= 1;
 
         // getting the negation of the variable
         int aNeg, bNeg;
-        if(a > n) aNeg = toTrue(a, n);
+        if(a >= n) aNeg = toTrue(a, n);
         else aNeg = toValidOrFalse(a, n);
-        if(b > n) bNeg = toTrue(b, n);
+        if(b >= n) bNeg = toTrue(b, n);
         else bNeg = toValidOrFalse(b, n);
         
-        std::cout << aNeg << " " << b << std::endl;
-        std::cout << bNeg << " " << a << std::endl;
-
         add_edge(aNeg, b, digraph);
         add_edge(bNeg, a, digraph);
     }
 
-    int *scc = new int[2*n]; memset(scc, -1, sizeof(*scc));
-    tarjan(digraph, scc);
-
-    bool sat = true;
-    int varUnsat;
-    for(int i = 1 ; i <= n ; i++){
-        int iNeg = toValidOrFalse(i, n);
-        if(scc[i] == scc[iNeg]){
-            varUnsat = i;
-            sat = false;
-            break;
+    for(Vertex vertex : make_iterator_range(vertices(digraph))){
+        std::cout << vertex << ": ";
+        for(Arc arc : make_iterator_range(out_edges(vertex, digraph))){
+            Vertex to = target(arc, digraph);
+            std::cout << to << " ";
         }
+        std::cout << std::endl;
     }
 
-    if(sat){
+    std::pair<bool, int> ans = tarjan(digraph);
+
+    if(ans.first){
         std::cout << "YES" << std::endl;
         // toDo
     } 
     else{
         std::cout << "NO" << std::endl;
-        std::cout << varUnsat << std::endl;
+        std::cout << ans.second << std::endl;
         // toDo
     }
+
 }
